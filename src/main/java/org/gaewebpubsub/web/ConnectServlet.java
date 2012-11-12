5439c7374632fc1ccdc571ff5b2e245700c772cd
Alex Devine <alex.devine@gmail.com>
2012-11-12 16:50:29 -0600

    Initial pass of all java servlets

--- src/main/java/org/gaewebpubsub/web/ConnectServlet.java
+++ src/main/java/org/gaewebpubsub/web/ConnectServlet.java
@@ -16,19 +16,35 @@
 package org.gaewebpubsub.web;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * TODO - comment
+ * ConnectServlet is called when a page includes the "javascript" to start up a connection to a topic. This servlet
+ * makes the connection and then returns javascript that the client can use to send and subscribe to messages.
  */
-public class ConnectServlet extends HttpServlet {
+public class ConnectServlet extends BaseServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/javascript");
         resp.setCharacterEncoding("UTF-8");
 
-        resp.getWriter().println("Hello world");
+        String topicKey = getRequiredParameter(req, TOPIC_KEY_PARAM, false /* can't be empty */);
+        String userKey = getRequiredParameter(req, USER_KEY_PARAM, false /* can't be empty */);
+        String userName = getRequiredParameter(req, USER_NAME_PARAM, false /* can't be empty */);
+        int topicLifetime = 120; //default 120 mins like the Channel API
+        try {
+            int topicLifetimeParamValue = Integer.parseInt(req.getParameter(TOPIC_LIFETIME_PARAM));
+            if (topicLifetimeParamValue > 0) {
+                topicLifetime = topicLifetimeParamValue;
+            }
+        } catch (Exception e) {
+            //OK, lifetime stays at the default
+        }
+
+        String userChannelToken = getTopicManager().connectUserToTopic(topicKey, userKey, userName, topicLifetime);
+
+        //TODO - forward onto script
+        resp.getWriter().println("User channel is " + userChannelToken);
     }
 }
